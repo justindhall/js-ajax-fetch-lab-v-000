8fee6690c21e3e111bbd83ff81f659661b15eeee
Justin Hall <hall.justin.d@gmail.com>
2019-02-15 22:01:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return 'fd63f6205406b58a95e378e997d9090cfc8b7134';
+  return '';
 }
 
 function forkRepo() {
@@ -24,8 +24,30 @@ function showResults(json) {
 
 function createIssue() {
   //use this function to create an issue based on the values input in index.html
+  const repo = `<INPUT-USERNAME>/js-ajax-fetch-lab`;
+  const url = `https://api.github.com/repos/${repo}/issues`;
+  const postData = {
+    title: document.getElementById('title').value,
+    body: document.getElementById('body').value
+  }
+
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(postData),
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.join()).then(json => getIssues());
 }
 
 function getIssues() {
   //once an issue is submitted, fetch all open issues to see the issues you are creating
+  const repo = `<INPUT-USERNAME>/js-ajax-fetch-lab`;
+  const url = `https://api.github.com/repos/${repo}/issues`;
+
+  fetch(url, {
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.join()).then(json => console.log(json));
 }
